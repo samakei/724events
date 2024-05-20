c154e695ddd5a4c8456b27ea56f0242cad6bbb9a
SAMAKE Issa <samakeissa10@outlook.fr>
2024-05-20 20:25:19 +0200

    ajout du progresion

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,7 +23,7 @@ const Page = () => {
         <Slider />
       </section>
       <section className="ServicesContainer">
-        <h2 className="Title">Nos services</h2>
+        <h2 className="Title" id="nos-services">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
           <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
@@ -52,14 +52,15 @@ const Page = () => {
         </div>
       </section>
       <section className="EventsContainer">
-        <h2 className="Title">Nos réalisations</h2>
-        <EventList />
+        <h2 className="Title" id="nos-realisations">Nos réalisations</h2>
+        <EventList  /> 
+        
       </section>
       <section className="PeoplesContainer">
-        <h2 className="Title">Notre équipe</h2>
+        <h2 className="Title" id="notre-equipe">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
-          <PeopleCard
+          <PeopleCard 
             imageSrc="/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png"
             name="Samira"
             position="CEO"
@@ -117,7 +118,7 @@ const Page = () => {
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
         {last && ( // Vérifie si last est défini avant de rendre l'EventCard
-        <EventCard
+        <EventCard 
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
